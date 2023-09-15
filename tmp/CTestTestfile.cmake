9da58fb8284fb663ad30edc3eab1830f6e1da81f
Panarin Ilya <cccthafcdh@gmail.com>
2023-09-15 14:04:09 +0700

    unit_test_for_1_lab

---
+++ tmp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ilya/Рабочий стол/с++
+# Build directory: /home/ilya/Рабочий стол/с++/tmp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
